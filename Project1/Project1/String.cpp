01347ff18d9ad00a1b016fe8721523b8fd35ea7e
Roger Nogue Ballbé <rogernogueb@gmail.com>
2018-10-08 13:07:20 +0200

    String class creator functions implemented

--- Project1/Project1/String.cpp
+++ Project1/Project1/String.cpp
@@ -1,26 +1,42 @@
 #include "String.h"
 
-
-
-String::String()
-{
+void String::copy(const char* str) {
+	start = new char;
+	size = 0;
+	const char* i = str; //the contents of str will mot be modified
+	char* iterator = start;
+	while (*i != 0) {
+		*iterator = *i;
+		++iterator;
+		++i;
+		++size;
+	}
+	++iterator;
+	*iterator = 0;
 }
 
-String::~String()
-{
+String::String(const char* str){
+	copy(str);
 }
 
+String::String(const String *s){
+	copy(s->start);
+}
+
+String::~String(){
+}
+/*
 String String::operator=(String a)
 {
-	return String();
+	//return String();
 }
 
 String String::operator==(String a)
 {
-	return String();
+	//return String();
 }
 
 String String::operator+(String a)
 {
-	return String();
-}
+	//return String();
+}*/
--- Project1/Project1/String.h
+++ Project1/Project1/String.h
@@ -1,13 +1,21 @@
 class String
 {
 public:
-	String();
+	String(const char *str);
+	String(const String *s);
 	~String();
 
 	//operator override
+	/*
 	String operator=(String a);
 	String operator+(String a);
 	String operator==(String a);
+	*/
+	//variables
+	char *start;
+	int size;
+private:
+	void copy(const char* str);
 
 };
 
